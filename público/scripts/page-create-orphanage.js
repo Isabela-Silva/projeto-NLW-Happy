89b5319f35233e90b8db885eed6c1272e0135079
Isabela Regina Silva <72450422+Isabela-Silva@users.noreply.github.com>
2020-12-09 11:31:10 -0300

    Update and rename public/scripts/page-create-orphanage.js to público/scripts/page-create-orphanage.js

--- público/scripts/page-create-orphanage.js
+++ público/scripts/page-create-orphanage.js
@@ -18,7 +18,7 @@ map.on("click", (event) => {
   const lng = event.latlng.lng;
 
   document.querySelector("[name=lat]").value = lat;
-  document.querySelector("[name=lng]").value = lat;
+  document.querySelector("[name=lng]").value = lng;
 
   //remove icon
   marker && map.removeLayer(marker);
